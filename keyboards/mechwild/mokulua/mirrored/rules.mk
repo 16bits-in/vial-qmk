eb32093dc68cf8d0a4170995352374e80d7c16f6
Ilya Zhuravlev <whatever@xyz.is>
2024-04-20 21:25:10 -0600

    fix default keymaps

--- keyboards/mechwild/mokulua/mirrored/rules.mk
+++ keyboards/mechwild/mokulua/mirrored/rules.mk
@@ -3,7 +3,7 @@
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
